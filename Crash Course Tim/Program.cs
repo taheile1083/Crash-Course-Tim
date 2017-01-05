fa3b58f63fc805efa276b0952961c19280609079
taheile1083 <theile1083@my.sctd.edu>
2017-01-05 10:04:14 -0500

    Added Decision Loop

--- Crash Course Tim/Program.cs
+++ Crash Course Tim/Program.cs
@@ -20,6 +20,14 @@ namespace Crash_Course_Tim
             Console.WriteLine("The value of int year is: " + year + punc);
             Console.WriteLine("The value of bool booring is: " + booring + punc);
             Console.WriteLine("The value of double pie is: " + pie + punc);
+            Console.WriteLine("Select a number 1, 2, or 3.");
+            string snum = Console.ReadLine();
+            int inum = Convert.ToInt32(snum);
+            if (inum == 1) {
+                Console.WriteLine("You Win!");
+            } else {
+                Console.WriteLine("You Lose!");
+            }
             Console.Read();
         }
     }
