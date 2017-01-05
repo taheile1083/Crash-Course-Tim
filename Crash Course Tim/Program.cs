057bdae991e9fe34533ebb05c0154e16af3daff8
taheile1083 <theile1083@my.sctd.edu>
2017-01-05 09:31:56 -0500

    Added Data Types

--- Crash Course Tim/Program.cs
+++ Crash Course Tim/Program.cs
@@ -10,7 +10,16 @@ namespace Crash_Course_Tim
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int year = 2017;
+            bool booring = true;
+            double pie = 6.283184;
+            char punc = '!';
+            string world = "Hello, World";
+            Console.WriteLine("The value of char punc is" + punc);
+            Console.WriteLine("The value of string world is: " + world +punc);
+            Console.WriteLine("The value of int year is: " + year + punc);
+            Console.WriteLine("The value of bool booring is: " + booring + punc);
+            Console.WriteLine("The value of double pie is: " + pie + punc);
             Console.Read();
         }
     }
