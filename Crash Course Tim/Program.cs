98c28ea690ec03085f865dd789f5ce8eba6eca9f
theile1083 <theile1083@my.sctd.edu>
2017-01-10 09:59:21 -0500

    FizzBuzz!

--- Crash Course Tim/Program.cs
+++ Crash Course Tim/Program.cs
@@ -10,23 +10,47 @@ namespace Crash_Course_Tim
     {
         static void Main(string[] args)
         {
-            int year = 2017;
-            bool booring = true;
-            double pie = 6.283184;
-            char punc = '!';
-            string world = "Hello, World";
-            Console.WriteLine("The value of char punc is" + punc);
-            Console.WriteLine("The value of string world is: " + world +punc);
-            Console.WriteLine("The value of int year is: " + year + punc);
-            Console.WriteLine("The value of bool booring is: " + booring + punc);
-            Console.WriteLine("The value of double pie is: " + pie + punc);
-            Console.WriteLine("Select a number 1, 2, or 3.");
-            string snum = Console.ReadLine();
-            int inum = Convert.ToInt32(snum);
-            if (inum == 1) {
-                Console.WriteLine("You Win!");
-            } else {
-                Console.WriteLine("You Lose!");
+            //int year = 2017;
+            //bool booring = true;
+            //double pie = 6.283184;
+            //char punc = '!';
+            //string world = "Hello, World";
+            //Console.WriteLine("The value of char punc is" + punc);
+            //Console.WriteLine("The value of string world is: " + world +punc);
+            //Console.WriteLine("The value of int year is: " + year + punc);
+            //Console.WriteLine("The value of bool booring is: " + booring + punc);
+            //Console.WriteLine("The value of double pie is: " + pie + punc);
+            //Console.WriteLine("Select a number 1, 2, or 3.");
+            //string snum = Console.ReadLine();
+            //int inum = Convert.ToInt32(snum);
+            //if (inum == 1) {
+            //    Console.WriteLine("You Win!");
+            //} else {
+            //    Console.WriteLine("You Lose!");
+            //}
+            //Console.Read();
+
+            int fizz;
+            int buzz;
+            string output;
+            for (int i = 1; i <= 100; i++)
+            {
+                fizz = i % 3;
+                buzz = i % 5;
+                output = i.ToString();
+                if (fizz == 0)
+                {
+                    output = "Fizz";
+                    if (buzz == 0)
+                    {
+                        output += "Buzz";
+                    }
+                }
+                else if (buzz == 0)
+                {
+                    output = "Buzz";
+                }
+                Console.WriteLine(output + "!");
             }
             Console.Read();
         }
