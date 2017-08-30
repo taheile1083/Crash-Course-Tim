e717e2673d8a101d0d4c8d10f5c4109d6e858f5c
Tim Heilers <theile1083@my.sctd.edu>
2017-08-30 19:33:24 -0400

    Added Circular Buffer Crash Course

---
+++ Circular Buffer/Circular Buffer/Buffer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circular_Buffer
+{
+    class Buffer<T>
+    {
+        public int Length { get; set; }
+        public int ReadAt { get; set; }
+        public int WriteAt { get; set; }
+        public T[] BuffArr { get; set; }
+
+        public Buffer(int L){
+            Length = L;
+            BuffArr = new T[Length];
+        }
+
+        public T Read()
+        {
+            T temp = BuffArr[ReadAt];
+            if (ReadAt != WriteAt)
+            {
+                ReadAt = ((ReadAt + 1) % Length);
+            }
+            return temp;
+        }
+        public bool Write(T input)
+        {
+            if (((WriteAt + 1) % Length) != ReadAt)
+            {
+                BuffArr[WriteAt] = input;
+                WriteAt = ((WriteAt + 1) % Length);
+                return true;
+            }
+            else return false;
+        }
+    }
+}
---
+++ Circular Buffer/Circular Buffer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circular_Buffer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Buffer<int> testbuffer = new Buffer<int>(5);
+            for (int i = 0; i < 20; i++)
+            {
+                testbuffer.Write(i);
+                Console.Write(testbuffer.ReadAt+"    ");
+                Console.Write(testbuffer.WriteAt+"    ");
+                Console.WriteLine(testbuffer.Read());
+            }
+            Console.Read();
+        }
+    }
+}
